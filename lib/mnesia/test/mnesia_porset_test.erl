ae7144b1b1668b0903494f4517461bd5cc5229d1
Shuntian Liu <shuntian.liu@outlook.com>
2023-03-05 11:07:17 +0000

    feat: causal and porset tests

---
+++ lib/mnesia/test/mnesia_porset_test.erl
@@ -0,0 +1,59 @@
+-module(mnesia_porset_test).
+
+-author('sl955@cam.ac.uk').
+
+-include("mnesia_test_lib.hrl").
+
+-export([init_per_testcase/2, end_per_testcase/2, init_per_group/2, end_per_group/2,
+         all/0, groups/0]).
+-export([match_delete_ram/1]).
+
+init_per_testcase(Func, Conf) ->
+    mnesia_test_lib:init_per_testcase(Func, Conf).
+
+end_per_testcase(Func, Conf) ->
+    mnesia_test_lib:end_per_testcase(Func, Conf).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+all() ->
+    [match_delete_ram].
+
+groups() ->
+    [{write_tests, [], [match_delete_ram]}].
+
+init_per_group(_GroupName, Config) ->
+    Config.
+
+end_per_group(_GroupName, Config) ->
+    Config.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+match_delete_ram(suite) ->
+    [];
+match_delete_ram(Config) when is_list(Config) ->
+    match_delete(Config, ram_copies).
+
+match_delete(Config, Storage) ->
+    Nodes = [_NodeA, NodeA1, NodeA2] = NodeNames = ?acquire_nodes(3, Config),
+    Tab = match_delete,
+    Def = [{Storage, NodeNames}, {type, porbag}, {attributes, [k, v]}],
+    ?match({atomic, ok}, mnesia:create_table(Tab, Def)),
+    Reader = fun() -> mnesia:read(Tab, a) end,
+    Writer = fun(K, V) -> mnesia:write({Tab, K, V}) end,
+    ObjectDeleter = fun() -> mnesia:delete_object({Tab, a, 1}) end,
+    ?match(ok,
+           mnesia:activity(sync_ec,
+                           fun() ->
+                              Writer(a, 1),
+                              Writer(a, 2)
+                           end)),
+    ?match([{Tab, a, 1}, {Tab, a, 2}], mnesia:activity(sync_ec, Reader)),
+    ?match([{Tab, a, 1}, {Tab, a, 2}],
+           rpc:call(NodeA1, mnesia, activity, [sync_ec, Reader])),    % %% Delete the record
+    ?match([{Tab, a, 1}, {Tab, a, 2}], rpc:call(NodeA2, mnesia, activity, [sync_ec, Reader])),
+    ?match(ok, mnesia:sync_ec(ObjectDeleter)),
+
+    ?match([{Tab, a, 2}], mnesia:async_ec(Reader)),
+    ?verify_mnesia(Nodes,
+                   []).%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
