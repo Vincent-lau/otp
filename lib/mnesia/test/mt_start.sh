04974f7b631791e17c3f3d1d82424b236369eea7
Shuntian Liu <shuntian.liu@outlook.com>
2023-05-01 21:59:28 +0100

    script:minor test script change for debug

--- lib/mnesia/test/mt_start.sh
+++ lib/mnesia/test/mt_start.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+ERL_TOP=~/proj/otp
+TYPE=debug
 top="$ERL_TOP/lib/mnesia"
 h=`hostname`
 p="-pa $top/examples -pa $top/ebin -pa $top/test -mnesia_test_verbose true"
@@ -10,10 +12,8 @@ erlcmd="erl -sname a $p $args -mnesia_test_timeout"
 erlcmd1="erl -sname a1 $p $args"
 erlcmd2="erl -sname a2 $p $args"
 
-cd $ERL_TOP && make mnesia && cd $top/test && \
+cd $top && ERL_TOP=$ERL_TOP TYPE=$TYPE make debug && cd $top/test && \
 ~/proj/otp/bin/erl -make && \
 ~/proj/otp/bin/erl -name a@127.0.0.1 -pa '/home/vincent/proj/inet_tcp_proxy/ebin' \
--pa $top/examples -pa $top/src -pa $top/ebin $top/test -debug 
-
-# -proto_dist inet_tcp_proxy
+-pa $top/examples -pa $top/src -pa $top/ebin $top/test -debug -proto_dist inet_tcp_proxy
 
